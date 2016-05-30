b209cb822e95ce13309af3e600b70702f08d411d
Jaswanth Jeenu <jsjchandra@gmail.com>
2016-05-30 19:05:07 +0530

    Initial Commit

---
+++ Page view and Sliding menu Demo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Page view and Sliding menu Demo
+//
+//  Created by Jaswanth Jeenu on 30/05/16.
+//  Copyright © 2016 Jaswanth Jeenu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Page view and Sliding menu Demo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Page view and Sliding menu Demo
+//
+//  Created by Jaswanth Jeenu on 30/05/16.
+//  Copyright © 2016 Jaswanth Jeenu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
